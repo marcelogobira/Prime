4c1ee5f4212be62bac38fdb7b1ad05527f31830b
marcelogobira <gobira.rj@gmail.com>
2021-09-22 21:15:47 -0300

    first commit

---
+++ PrimeGenerator.cs
@@ -0,0 +1,62 @@
+using System;
+
+public class PrimeGenerator
+{
+    public static int[] GeneratePrimeNumbers(int maxValue)
+    {
+        if(maxValue >= 2)
+        {
+            int s = maxValue + 1; //array size
+            bool[] f = new bool[s];
+            int i;
+
+            for(i = 0; i < s; i++)
+            {
+                f[i] = true;
+            }
+
+            //discards known not primes
+            f[0] = f[1] = false;
+
+            int j;
+            for(i = 2; i < Math.Sqrt (s) + 1; i++)
+            {
+                if(f[i])
+                {
+                    for(j = 2 * i; j < s; j +=i)
+                    {
+                        f[j] = false; //the multiple is not a prime
+                    }
+                }
+            }
+
+
+            //how many prime numbers exists?
+            int count = 0;
+            for(i = 0; i < s; i++)
+            {
+                if(f[i])
+                {
+                    count++; //increments count
+                }
+            }
+
+            int[] primes = new int[count];
+
+            //moves prime numbers to result
+            for(i = 0, j = 0; i < s; i++)
+            {
+                if(f[i]) //if is prime
+                {
+                    primes[j++] = i;
+                }
+            }
+
+            return primes;
+        }
+        else
+        {
+            return new int[0]; //returns null array if invalid input
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var primes = PrimeGenerator.GeneratePrimeNumbers(100);
+
+            for(var i = 0; i < primes.Length; i++)
+            {
+                Console.WriteLine(primes[i]);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
