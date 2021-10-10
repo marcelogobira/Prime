861a43d5665327c463e1abb3417d34ef1cdaf926
marcelogobira <gobira.rj@gmail.com>
2021-10-10 15:28:15 -0300

    Prime generator v2

--- PrimeList.Core/PrimeGenerator.cs
+++ PrimeList.Core/PrimeGenerator.cs
@@ -4,62 +4,63 @@ namespace PrimeList
 {
     public class PrimeGenerator
     {
+        private static int s;
+        private static bool[] f;
+        private static int[] primes;
+
         public static int[] GeneratePrimeNumbers(int maxValue)
         {
-            if(maxValue >= 2)
+            if(maxValue < 2)
             {
-                int s = maxValue + 1; //array size
-                bool[] f = new bool[s];
-                int i;
-
-                for(i = 0; i < s; i++)
-                {
-                    f[i] = true;
-                }
-
-                //discards known not primes
-                f[0] = f[1] = false;
-
-                int j;
-                for(i = 2; i < Math.Sqrt (s) + 1; i++)
-                {
-                    if(f[i])
-                    {
-                        for(j = 2 * i; j < s; j +=i)
-                        {
-                            f[j] = false; //the multiple is not a prime
-                        }
-                    }
-                }
-
-
-                //how many prime numbers exists?
-                int count = 0;
-                for(i = 0; i < s; i++)
-                {
-                    if(f[i])
-                    {
-                        count++; //increments count
-                    }
-                }
-
-                int[] primes = new int[count];
-
-                //moves prime numbers to result
-                for(i = 0, j = 0; i < s; i++)
-                {
-                    if(f[i]) //if is prime
-                    {
-                        primes[j++] = i;
-                    }
-                }
-
-                return primes;
+                return new int[0];
             }
-            else
+
+            InitializeSieve(maxValue);
+            Sieve();
+            LoadPrimes();
+            return primes;
+        }
+
+        private static void LoadPrimes()
+        {
+            int i;
+            int j;
+
+            //How many prime numbers exists?
+            int count = 0;
+
+            for(i = 0; i < s; i++)
             {
-                return new int[0]; //returns null array if invalid input
+                if(f[i])
+                {
+                    count++; //increase count
+                }
             }
+
+            primes = new int[count];
+
+            for(i = 0, j = 0; i < s; i++)
+            {
+                if(f[i])
+                {
+                    primes[j++] = i;
+                }
+            }
+        }
+
+        private void InitializeSieve(int maxValue)
+        {
+            s = maxValue + 1;
+            f = new bool[s];
+            int i;
+
+            for(i = 0; i < s; i++)
+            {
+                f[i] = true;
+            }
+
+            //discard known primes
+            f[0] = f[1] = false;
         }
     }
 }
